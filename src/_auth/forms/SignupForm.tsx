685cb41512321e306c09e4025868233bbc5c963b
ncsaszar <ncsaszar97@gmail.com>
2023-11-13 12:27:36 -0500

    assets

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
+import { Button } from '@/components/ui/button';
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default SignupForm;
